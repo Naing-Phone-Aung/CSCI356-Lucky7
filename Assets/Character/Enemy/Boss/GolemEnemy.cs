8e5a06dd0f3357fcd2c3e2ef111f0789c23b52b1
Naing Phone Aung <112563025+Naing-Phone-Aung@users.noreply.github.com>
2024-08-27 13:21:38 +0800

    Naing Update

--- Assets/Character/Enemy/Boss/GolemEnemy.cs
+++ Assets/Character/Enemy/Boss/GolemEnemy.cs
@@ -1,13 +1,12 @@
-using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class GolemEnemy : MonoBehaviour
 {
-    Combat playerInfo;
-
+    //PlayerController myController;
     public Animator golemAnimator;
     public NavMeshAgent golemAgent;
 
@@ -19,70 +18,88 @@ public class GolemEnemy : MonoBehaviour
     public int throwDistance;
     public int nearDistance;
 
+    // Stone info
     public Transform stoneSpawnPoint;
     public GameObject stonePrefab;
     GameObject stoneClone;
 
+    // Explosion info
+    GameObject explosionClone;
+    public GameObject explosionPrefab;
+    public Transform explosionSpawnPoint;
+
+    // Spiders
+    public GameObject spiderPrefab;
+    GameObject spiderClone;
+    public Transform spiderSpawnPoint;
+    //public SpiderEnemy spiderEnemy;
+
+    // Start is called before the first frame update
     void Start()
     {
         golemAnimator = GetComponent<Animator>();
         golemAgent = GetComponent<NavMeshAgent>();
+        //myController = FindObjectOfType<PlayerController>();
+        //spiderEnemy = FindObjectOfType<SpiderEnemy>();
+        // spiderEnemy.enabled = false;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        StartFight();
+        startFight();
     }
 
-    public void StartFight()
+    public void startFight()
     {
         distanceToPlayer = Vector3.Distance(playerPosition.position, transform.position);
 
         if (distanceToPlayer <= distance)
         {
-            if (golemHealth <= 0)
+            if (distanceToPlayer >= stopDistance + 2)
             {
+                golemAgent.isStopped = false;
+                golemAnimator.SetInteger("State", 1);
+                golemAgent.SetDestination(playerPosition.position);
+            }
+            else if (golemHealth <= 0)
+            {
+                // Golem is dead, handle death (if any logic is required)
+                golemAnimator.SetInteger("State", 5);
+            }
+            else if (distanceToPlayer <= nearDistance && golemHealth <= 20 && golemHealth >= 15)
+            {
+                // Roar and spider appear
                 golemAgent.isStopped = true;
-                golemAnimator.SetInteger("State", 5); // Death state
+                golemAnimator.SetInteger("State", 4);
+                //spiderEnemy.enabled = true;
             }
             else if (distanceToPlayer <= nearDistance)
             {
+                // Attack near (flexing muscle)
                 golemAgent.isStopped = true;
-                golemAnimator.SetInteger("State", 3); // Near attack state
+                golemAnimator.SetInteger("State", 3);
             }
             else if (distanceToPlayer < stopDistance && golemHealth <= 40)
             {
+                // Throw with stone distance
                 golemAgent.isStopped = true;
                 transform.LookAt(playerPosition.position);
-                golemAnimator.SetInteger("State", 2); // Throw stone state
+                golemAnimator.SetInteger("State", 2);
             }
-            else if (distanceToPlayer >= stopDistance + 2)
-            {
-                golemAgent.isStopped = false;
-                golemAnimator.SetInteger("State", 1); // Move towards player
-                golemAgent.SetDestination(playerPosition.position);
-            }
-            else
-            {
-                golemAgent.isStopped = true;
-                golemAnimator.SetInteger("State", 0); // Idle state
-            }
-        }
-        else
-        {
-            golemAgent.isStopped = true;
-            golemAnimator.SetInteger("State", 0); // Idle state when out of range
         }
     }
 
     public void ThrowStone()
     {
         stoneClone = Instantiate(stonePrefab, stoneSpawnPoint.position, stoneSpawnPoint.rotation);
-        Destroy(stoneClone, 5);
+        GameObject.Destroy(stoneClone, 5);
     }
 
     public void AttackToPlayer()
     {
-        // Handle player attack logic
+        //myController.playerHealth--;
     }
+
+
 }
